e6f994176b0f618613a5c65b33c73bc89f5e18f6
gvemulapalli33 <gvemulapalli33@gmail.com>
2020-12-27 10:47:15 -0800

    Add menu functionality

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const categories = ['Local News', 'Technology', 'Entertainment', 'Science', 'Health'];
+const icons =['📰', '⚙️', '🎬',  '🚀', '🩺'];
+function Menu() {
+    const menuItems = categories.map((category, index) => {
+        return <li><span className="icon">{icons[index]}</span>{category}</li>
+    })
+    return (
+        <ul className="categories">
+          {menuItems}
+        </ul>
+    );
+}
 
 export default Menu;
